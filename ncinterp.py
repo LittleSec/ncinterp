688a1ca095e44225471b33a65342a354fcdbef47
小婊蜜 <517862788@qq.com>
2018-02-11 15:51:22 +0800

    新增DataSelector类，调整NcFile类方法的访问权限，英语化部分注释。

--- ncinterp.py
+++ ncinterp.py
@@ -8,84 +8,94 @@ import re
 import time
 
 class NcFile:
-	def __init__(self, fileName, rootPath):
+	def __init__(self, fileName, rootPath = r'/Users/littlesec/Desktop/毕业设计/caoweidongdata/'):
 		self.rootPath = rootPath
 		self.fileName = fileName
-		self.openAnNcFile()
+		self.f = self.__openAnNcFile()
+
+	def __openAnNcFile(self):
+		'''
+		This function is called by __init__(), can't called by other.
+		And will return a object<scipy.io.netcdf.netcdf_file>.
+		If fail to open, then return None.
+		'''
+		try:
+			os.chdir(self.rootPath) #进入工作目录
+			return netcdf.netcdf_file(self.fileName, 'r') #使用scipy自带nc文件读写方法
+		except FileNotFoundError as err:
+			print(err, '\nPlease check your directory and file are both existed!')
+			return None
 
 	def getFileInfo(self):
-		# 获取文件信息
-		self.getLongiAndLati()
-		self.getObservedValue()
-		self.dimensions = self.f.dimensions # dimensions不要用于获取某个维度的大小，
-											# nc文件在创建时很有可能在这里记录错误，例如sst2960-2017.nc这个文件的'T'维度大小是None
-											# 要想获得数据的数量，还是以实际数据的列表长度为准
-		self.getTimeValue()
-		if(len(self.dimensions) == 4):
+		'''
+		The information is included:
+				longitude and latitude(type: numpy.ndarray, dimension: 1)
+				observed value(type: numpy.ndarray)
+				observed value's demension: 3 or 4 
+				missingValue(must float, if the file doesn't define, it will prompt user to input)
+		'''	
+		self.x, self.y = self.getLongiAndLati()
+		self.getObservedValue() # will get missing_value, too
+		self.dimension = len(self.f.dimensions) 
+		# f.dimensions不要用于获取某个维度的大小，
+		# nc文件在创建时很有可能在这里记录错误，例如sst1960-2017.nc这个文件的'T'维度大小是None
+		# 要想获得数据的数量，还是以实际数据的列表长度为准
+		self.timeListself.getTimeValue()
+		if(self.dimension == 4):
 			self.getDepthValue()
 		if np.isnan(self.missingValue):
 			print("There is no missing_value in this file: " + self.fileName)
+			print("The observed value: **max: {0}**, **min: {1}**".format(np.nanmax(self.observedValue), np.nanmin(self.observedValue)))
 			self.missingValue = float(eval(input("Please input a num to replace the missing_value: ")))
-		'''
-		# 下面获取数据的组数，注意有依赖关系即必须先获得观测值。
-		num = 1
-		for i in range(len(self.observedValue.shape)-2): # 默认后两个属性对应的值是经纬度
-			num *= self.observedValue.shape[i]		
-		self.numOfGroups = num # 数据的组数
-		'''
-
-	def openAnNcFile(self, fileName = None):
-		os.chdir(self.rootPath) #进入工作目录
-		if fileName is None:
-			fileName = self.fileName
-		self.f = netcdf.netcdf_file(fileName, 'r') #使用scipy自带nc文件读写方法
 
 	def getLongiAndLati(self):
-		'''获取经纬度，因为并非所有文件的经纬度索引key都是X和Y，因此需要通过long_name遍历搜索
-		同时有一个文件的所有属性都没有long_name，因此通过两种方法进行获取'''
-		self.x = []
-		self.y = []
+		'''
+		This function will return two numpy.ndarrays,
+		one is longitude(x), another is latitude(y).
+		By the way, there is no code to handle date reading error.
+		In other words, it's successfully get date by default.
+		'''
+		if self.f is None:
+			print("Error: There is no file opening.")
+			return
+		x = []
+		y = []
+		# 直接用正则表达式匹配X,Y,lat,lon,latitude,longitude
 		for key in self.f.variables.keys():
-			try: # 有些属性没有long_name，会引发异常
-				if(self.f.variables[key].long_name == b"Longitude"):
-					#self.x = [x for x in self.f.variables[key].data if 117.5 <= x <= 129.5]
-					self.x = self.f.variables[key].data
-				elif(self.f.variables[key].long_name == b"Latitude"):
-					self.y = self.f.variables[key].data
-			except Exception as e:
-				print(key, "the error message is ", e)
-		if(len(self.x) == 0):
-			self.x = self.f.variables['X'].data
-		if(len(self.y) == 0):
-			self.y = self.f.variables['Y'].data
+			if(len(x) != 0 and len(y) != 0):
+				break
+			if key.lower() in ['lon', 'x', 'longitude']:
+				x = self.f.variables[key].data
+				#self.x = [x for x in self.f.variables[key].data if 117.5 <= x <= 129.5]
+			elif key.lower() in ['lat', 'y', 'latitude']:
+				y = self.f.variables[key].data
+		return x, y
 
 	def getTimeValue(self):
-		'''获取时间序列'''
+		'''
+		This function will read the time(actually it's the date) list in nc file, and return it.
+		In nc file, the item in time list is just a number started from 1 or 0.5, and it's not esay for us to read them intuitively.
+		So, this function will change number to a date string(yyyy-mm-dd) by combining with the units.
+		Attention, by default, every month just has 30 days.
+		'''
 		dateNumberList = []
 		dateUnits = ''
-		for keys in self.f.variables.keys():
-			try: # 有些属性没有long_name，会引发异常
-				if(self.f.variables[keys].long_name == b"Time"):
-					dateNumberList = self.f.variables[keys].data # 仅仅是一堆数字，不能直观反映绝对时间
-					dateUnits = str(self.f.variables[keys].units, encoding = 'utf-8') # 单位
-					break
-			except Exception as e:
-				print("the error message is ", e)
-		if(dateUnits == ''):
-			dateNumberList = self.f.variables['T'].data
-			dateUnits = str(self.f.variables['T'].units, encoding = 'utf-8')
+		for key in self.f.variables.keys():
+			if key.lower() in ['time', 't']:
+				dateNumberList = self.f.variables[key].data
+				dateUnits = str(self.f.variables[key].units, encoding = 'utf-8')
+				break
 
-		self.timeList = [] # 该序列每个元素都是时间直观的字符串
-		startDate = dateUnits.split('since')[-1] # 最后的子串是开始日期
+		dateStringList = [] # 该序列每个元素都是时间直观的字符串
+		startDate = dateUnits.split('since')[-1].strip() # 最后的子串是开始日期
 		if(dateUnits.split()[0] == 'months'): # 单位是月，但相加的值可能是小数，因此要分年月日处理
-			startDate = startDate.strip()
 			startDateSubList = startDate.split('-')
 			startYear = int(startDateSubList[0])
 			startMonth = int(startDateSubList[1])
 			startDay = int(startDateSubList[2])
 			for i in range(len(dateNumberList)):
 				intPart = int(dateNumberList[i])
-				decimalPart = dateNumberList[i] - intPart
+				decimalPart = round(dateNumberList[i] - intPart, 1) # just try 1.9-1
 				day = startDay + int(decimalPart * 30)
 				month = startMonth
 				year = startYear
@@ -94,29 +104,37 @@ class NcFile:
 					month += 1
 				month += intPart
 				if(month > 12):
-					month %= 12
 					year += (month // 12)
+					month %= 12 # Can't invert the order, because this word will change month value
+					if(month == 0): # think about e.g. 23.5, month=1+23=24, but 24/12=2...0, in fact year just +1 and month should be 12
+						month = 12
+						year -= 1
 				date = '{:>4}-{:0>2}-{:0>2}'.format(year, month, day)
-				self.timeList.append(date)
+				dateStringList.append(date)
 		elif(dateUnits.split()[0] == 'days'):
-			startDate = startDate.strip()[:-10] # 最后两位是小数点
+			startDate = startDate[:-10] # 舍去后面的时间00:00:0.0
 			d = datetime.datetime.strptime(startDate, '%Y-%m-%d')
 			for i in range(len(dateNumberList)):
 				delta = datetime.timedelta(days=int(dateNumberList[i]))
 				date = d + delta
-				self.timeList.append(str(date.strftime('%Y-%m-%d')))
+				dateStringList.append(str(date.strftime('%Y-%m-%d')))
+		return dateStringList
+
 
 	def getDepthValue(self):
-		self.depthDataList = self.f.variables['Z'].data
-		self.depthUnits = str(self.f.variables['Z'].units, encoding = 'utf-8')
-
+		for key in self.f.variables.keys():
+			if key.lower() in ['depth', 'z']:
+				self.depthDataList = self.f.variables[key].data
+				self.depthUnits = str(self.f.variables[key].units, encoding = 'utf-8')
+				break
+		
 	def getObservedValue(self):
-		'''获取所有实际测量的值，默认属性名不用以下名字：['X', 'Y', 'Z', 'T', 'lat', 'lon']'''
-		for keys in self.f.variables.keys():
-			if(keys in ['T', 'X', 'Y', 'Z', 'lat', 'lon', 'time']):
+		'''获取所有实际测量的值，默认属性名不用以下名字：['X', 'Y', 'Z', 'T', 'lat', 'lon', 'depth']'''
+		for key in self.f.variables.keys():
+			if(key in ['T', 'X', 'Y', 'Z', 'lat', 'lon', 'time', 'depth']):
 				continue # 利用短路运算提高速度，保证for循环中有且仅有一次是遍历整个list
 			else:
-				var = self.f.variables[keys]
+				var = self.f.variables[key]
 				self.observedValue = var.data.astype(float)
 				self.missingValue = np.float(var.missing_value)
 				if('add_offset' in dir(var) and 'scale_factor' in dir(var)): # 源数据经过处理，例如sst2960-2017.nc
@@ -131,12 +149,12 @@ class NcFile:
 		若是除了经纬度之外，还有时间和深度两个变量，则经纬度的索引分别是X,Y，而深度的索引是Z，时间的索引是T
 		若是除了经纬度之外，只有时间这个变量，则经纬度的索引分别是lon,lat，时间的索引是time
 		观测值的维度由外到内分别是：时间、深度（如果有的话）、经纬度'''
-		if(len(self.dimensions) == 4):
+		if(self.dimension == 4):
 			data = DataProcessor(self.x, self.y, self.observedValue[timeIndex][depthIndex], dataInfo)
 		else:
 			data = DataProcessor(self.x, self.y, self.observedValue[timeIndex], dataInfo)
 		data.setNaNValue(50)
-		data.setResolution(minutes)
+		data.setResolution(minutes = minutes)
 		data.setInterpFunc()
 		data.interpAGroupData()
 		data.writeInCSV()
@@ -148,7 +166,7 @@ class NcFile:
 		dataInfo['RootPath'] = self.rootPath
 		dataInfo['DirsName'] = self.fileName[:-3]
 		dataInfo['MissingValue'] = self.missingValue
-		if(len(self.dimensions) == 4):
+		if(self.dimension == 4):
 			for i in range(len(self.timeList)):
 				dataInfo['Time'] = self.timeList[i]
 				for j in range(len(self.depthDataList)):
@@ -160,6 +178,146 @@ class NcFile:
 				dataInfo['Time'] = self.timeList[i]
 				self.processAGroupData(dataInfo, i, 0, minutes)
 
+class DataSelector:
+	def __init__(self, dateStringList, depthList):
+		self.startDate = dateStringList[0]# '1960-02-16' # 
+		self.endDate = dateStringList[len(dateStringList)-1]# '2008-06-17' # 
+		self.dateStringList = dateStringList # ['1960-02-16', '1960-03-16', '1960-04-16', '1961-05-16', '1970-06-16', '2001-07-16', '2008-06-16']
+		self.depthList = depthList # [1, 2.3, 3.45, 4.567, 5.65, 6.3, 7]
+
+	def __indexOfTime(self, timeStr):
+		'''
+		This function will return a index or timeStr in dateStringList.
+		The format of value in dateStringList(like startDate, endDate) is yyyy-mm-dd, but the format of timeStr is yyyymm.
+		By the way, this is a private function, the caller will ensure the format of timeStr is legal.
+		If timeStr earlier than self.startDate, then return 0(the first index).
+		Similarly, if timeStr later than self.endDate, then return the last index(length-1)
+		By default, the dateStringList is an ascending and continuous-months sequence, and have't same date.
+		'''
+		startYear = int(self.startDate.split('-')[0])
+		startMonth = int(self.startDate.split('-')[1])
+		endYear = int(self.endDate.split('-')[0])
+		endMonth = int(self.endDate.split('-')[1])
+		year = int(timeStr[:4])
+		month = int(timeStr[-2:])
+		if(year < startYear):
+			return 0 # 1960-01
+		elif(year == startYear):
+			if(month < startMonth):
+				return 0
+			else:
+				return month - startMonth
+		elif(year > endYear or (year == endYear and month > endMonth)):
+			return len(self.dateStringList) - 1 # 2008-12
+		else:
+			return (year - startYear) * 12 + (month - 1)
+
+	def selectByTime(self, timeStr):
+		'''
+		param: 
+			timeStr can be："yyyy", "yyyymm" or "str1-str2"(str1 and str2 must be yyyymm or yyyy).
+			As you think, yyyy alone means yyyy01-yyyy12 and in str1 means yyyy01, in str2 means yyyy12.
+		Function:
+			This function will retuan a list of time index.
+			If param is ''
+			If the format of timeStr is illegal, than return 0
+		'''
+		ymPattern = r'([12]\d{3})(0[1-9]|1[0-2])$' # yyyymm
+		ymRegex = re.compile(ymPattern)
+
+		yPattern = r'[12]\d{3}$' # yyyy
+		yRegex = re.compile(yPattern)
+
+		strSplitList = timeStr.split('-')
+		if len(strSplitList) == 1: # no '-'
+			if ymRegex.match(timeStr):
+				index1 = index2 = self.__indexOfTime(timeStr)
+			elif yRegex.match(timeStr): # Can't invert the order, because yyyymm is match yyyy, too.
+				index1 = self.__indexOfTime(timeStr + '01')
+				index2 = self.__indexOfTime(timeStr + '12') # Can't just +12, because +12 may index out of range
+			else:
+				index1 = index2 = 0
+		elif len(strSplitList) == 2: # can be 'str1-str2', 'str1-', '-str2'
+			if ymRegex.match(strSplitList[0]):
+				index1 = self.__indexOfTime(strSplitList[0])
+			elif yRegex.match(strSplitList[0]):
+				index1 = self.__indexOfTime(strSplitList[0] + '01')
+			else:
+				index1 = 0
+
+			if ymRegex.match(strSplitList[1]):
+				index2 = self.__indexOfTime(strSplitList[1])
+			elif yRegex.match(strSplitList[1]):
+				index2 = self.__indexOfTime(strSplitList[1] + '12')
+			else: # included the case like '201010-'
+				index2 = self.__indexOfTime(self.endDate.replace('-','')[:-2])
+		else:
+			index1 = index2 = 0
+
+		if index1 > index2:
+			index1, index2 = index2, index1
+
+		return list(range(index1, index2 + 1))
+
+	def __indexOfDepth(self, num, depthList=None):
+		'''
+		The main idea is bin_search.
+		If find successfully, than return the index.
+		If find unsuccessfully，than return the index whose value is closer to num.
+		By the way, depthList is an ascending sequence by default and haven't same value.
+		'''
+		depthList = self.depthList
+		low = 0
+		high = len(depthList) - 1
+		if num <= depthList[0]:
+			return 0
+		elif num >= depthList[len(depthList)-1]:
+			return len(depthList)-1
+		while low <= high:
+			mid = (low + high) // 2
+			if depthList[mid] == num:
+				return mid
+			elif depthList[mid] > num:
+				high = mid - 1
+			else:
+				low = mid + 1
+		if depthList[low] - num >= num - depthList[high]: # now the depthList[low] > depthList[high] 
+			return high
+		else:
+			return low
+
+	def selectByDepth(self, depthStr):
+		'''
+		param:
+			depthStr can be: "num(+)", "num1-num2"(num1 < num2) and "num-"
+		'''
+		strSplitList = depthStr.split('-')
+		numPattern = '-?(([1-9]\d*\.\d*|0?\.\d*[1-9]\d*|0?\.0+|0)|([1-9]\d*))'
+		numRegex = re.compile(numPattern)
+
+		if len(strSplitList) == 1:
+			if numRegex.match(depthStr):
+				index1 = index2 = self.__indexOfDepth(eval(depthStr))
+			else:
+				index1 = index2 = 0
+		elif len(strSplitList) == 2:
+			if numRegex.match(strSplitList[0]):
+				index1 = self.__indexOfDepth(eval(strSplitList[0]))
+			else:
+				index1 = 0
+
+			if numRegex.match(strSplitList[1]):
+				index2 = self.__indexOfDepth(eval(strSplitList[1]))
+			else:
+				index2 = self.__indexOfDepth(99999) # hard code: max num
+
+		if index1 > index2:
+			index1, index2 = index2, index1
+
+		return list(range(index1, index2 + 1))
+
+	def selectByTimeAndDepth(self, time, depth):
+		pass
 
 class DataProcessor:
 	def __init__(self, x, y, z, dataInfo):
@@ -187,13 +345,10 @@ class DataProcessor:
 				if np.isnan(self.z[i][j]) or self.z[i][j] == self.NaNValue:
 					self.z[i][j] = NaNReplaceValue	
 
-	def setResolution(self, minutes = 5):
+	def setResolution(self, min_x = 117.5, max_x = 129.5, min_y = 24.5, max_y = 36.5, minutes = 5):
 		'''设置分辨率，minutes指多少分就有一个数据'''
 		numOfSectionsInOneDegree = 60 // minutes
-		max_x = 129.5#np.max(self.x)#
-		min_x = 117.5#np.min(self.x)#
-		max_y = 36.5#np.max(self.y)#
-		min_y = 24.5#np.min(self.y)#
+		# max_x = np.max(self.x)#
 		n_x = complex(0, (max_x - min_x) * numOfSectionsInOneDegree + 1)
 		n_y = complex(0, (max_y - min_y) * numOfSectionsInOneDegree + 1)
 		self.xi = np.ogrid[min_x:max_x:n_x]
@@ -222,16 +377,14 @@ if __name__ == '__main__':
 	f1.processAFileData()
 	'''
 	start = time.clock()
-	rootPath = r'/Users/littlesec/Desktop/ncinterp/'
+	rootPath = r'/Users/littlesec/Desktop/毕业设计/caoweidongdata/'
 	os.chdir(rootPath) #进入工作目录
 	fileNamePattern = re.compile(r'.*.nc') # 第一个点不能省略
 	fileList = os.listdir()
 	for file in fileList:
 		if not fileNamePattern.match(file) is None:
-		# if file in ['relative humidity1960-2017.nc', 'sea level pressure1960-2017.nc', 'sst1960-2017.nc', 'uwind1960-2017.nc', 'vwind1960-2017.nc']:
 			f1 = NcFile(file, rootPath)
 			f1.processAFileData(5)
 
 	elapsed = (time.clock()-start)
 	print("run time: "+str(elapsed)+" s")
-		
