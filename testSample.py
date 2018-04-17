c19ee86674fb31d8783873eae1828a2f89e01d2d
小婊蜜 <517862788@qq.com>
2018-04-17 16:13:47 +0800

    测试样例代码。

--- testSample.py
+++ testSample.py
@@ -1,19 +1,20 @@
 import ncinterp as ni
-import okuboweiss as ow
+# import okuboweiss as ow
 rootPath = r'/Users/littlesec/Desktop/毕业论文实现/SODA v2p2p4 new'
-file = ni.NcFile('1000m_zonal_velocity1960-2008.nc', rootPath)
+file = ni.NcFile('200m_zonal_velocity1960-2008.nc', rootPath)
 file.getFileInfo()
 ds = ni.DataSelector(file.dateStrList, file.depthDataList)
-ds.selectByDepth('-400') # param can be '5-100' or '-100' or '100-' or '100'
+ds.selectByDepth('10') # param can be '5-100' or '-100' or '100-' or '100'
 ds.selectByTime('2000-') # param can be 'yyyy' or 'yyyymm' or 't-t'(t can be yyyy or yyyymm or ignore one) 
 ni.ncToCSVgrid(file, ds)
-path = rootPath + '/' + '1000m_zonal_velocity1960-2008_grid_(33x25)'
-ni.interpAFolder(path, 1)
-file = ni.NcFile('1000m_meridional_velocity1960-2008.nc', rootPath)
+path = rootPath + '/' + '200m_zonal_velocity1960-2008_grid_(33x25)'
+ni.interpAFolder(path, 10)
+
+file = ni.NcFile('200m_meridional_velocity1960-2008.nc', rootPath)
 file.getFileInfo()
 ni.ncToCSVgrid(file, ds)
-path = rootPath + '/' + '1000m_meridional_velocity1960-2008_grid_(33x25)'
-ni.interpAFolder(path, 1)
-uFolder = '1000m_zonal_velocity1960-2008_grid_(33x25)'
-vFolder = '1000m_meridional_velocity1960-2008_grid_(33x25)'
-ow.processAFolderOW(rootPath,uFolder,vFolder)
+path = rootPath + '/' + '200m_meridional_velocity1960-2008_grid_(33x25)'
+ni.interpAFolder(path, 10)
+# uFolder = '200m_zonal_velocity1960-2008_grid_(33x25)'
+# vFolder = '200m_meridional_velocity1960-2008_grid_(33x25)'
+# ow.processAFolderOW(rootPath,uFolder,vFolder)
