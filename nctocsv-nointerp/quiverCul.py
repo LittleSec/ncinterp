9a931e1ba109b515e51503fc7ab8882740de6693
openmac <517862788@qq.com>
2018-05-17 12:58:56 +0800

    quiver扩大比例因子，修改精度

--- nctocsv-nointerp/quiverCul.py
+++ nctocsv-nointerp/quiverCul.py
@@ -3,8 +3,8 @@ import pandas as pd
 import os
 import time
 
-RESOLUTION = 0.5 # 2/25*6 --> 0.5 --> 0.6 # 该数值应该通过实验修改
-ROOTPATH = '/Users/openmac/Downloads/'
+RESOLUTION = 1 # 2/25*6 * 2 --> 1 # 该数值应该通过实验修改
+ROOTPATH = '/Users/openmac/Downloads/LittleSec/new nc data/速度矢量场数据/'
 DEPTHLIST = ['0.0m', '8.0m', '15.0m', '30.0m', '50.0m']
 ATTRLIST = ['water_u', 'water_v']
 
@@ -13,14 +13,17 @@ def culEndPoint(absfileName):
     param:
         absfileName: tuple csv文件的绝对地址
         绘制quiver图的箭头，svg绘制线箭头需要确定起终点，该函数就是根据uv算出理论上终点的经纬度
+    attention:
+        c的float保证6位有效
+        为了提高前段计算效率, 最后存储时最多保留6位有效
     '''
     df = pd.read_csv(absfileName)
-    maxuv = np.max(np.abs(df[['water_u', 'water_v']].values))
+    maxuv = np.round(np.max(np.abs(df[['water_u', 'water_v']].values)), 6)
     scaleFactor = RESOLUTION/maxuv
     df['lon1'] = df['lon'] + df['water_u'] * scaleFactor
     df['lat1'] = df['lat'] + df['water_v'] * scaleFactor
-    df['scalar'] = np.sqrt(df['water_u']**2 + df['water_v']**2)
-    df.to_csv(absfileName, index=False, na_rep='NaN')
+    df['scalar'] = np.sqrt(df['water_u'].round(6)**2 + df['water_v'].round(6)**2)
+    df.round(6).to_csv(absfileName, index=False, na_rep='NaN')
 
 def sparseResolution(srcFile, srcPath, tarPath, reso=6):
     '''
@@ -54,20 +57,20 @@ if __name__ == '__main__':
     start = time.clock()
     os.chdir(ROOTPATH)
     # 稀疏化密度
-    for attr in ATTRLIST:
-        for depth in DEPTHLIST:
-            srcPath = '/'.join([attr+'_grid', depth])
-            tarPath = '/'.join(['s_'+attr+'_grid', depth])
-            os.makedirs(tarPath)
-            for file in os.listdir(srcPath):
-                sparseResolution(file, srcPath, tarPath)
-        print("run time: "+str(time.clock()-start)+" s")
+    # for attr in ATTRLIST:
+    #     for depth in DEPTHLIST:
+    #         srcPath = '/'.join([attr+'_grid', depth])
+    #         tarPath = '/'.join(['s_'+attr+'_grid', depth])
+    #         os.makedirs(tarPath)
+    #         for file in os.listdir(srcPath):
+    #             sparseResolution(file, srcPath, tarPath)
+    #     print("run time: "+str(time.clock()-start)+" s")
         
-
     # 计算quiver终点
-    # for depth in DEPTHLIST:
-    #     if os.path.isdir(depth):
-    #         for file in os.listdir(depth):
-    #             if file[-4:] == '.csv':
-    #                 culEndPoint('/'.join([depth, file]))
-    # print("run time: "+str(time.clock()-start)+" s")
+    for depth in DEPTHLIST:
+        if os.path.isdir(depth):
+            for file in os.listdir(depth):
+                if file[-4:] == '.csv':
+                    culEndPoint('/'.join([depth, file]))
+        print("run time: "+str(time.clock()-start)+" s")
+        start = time.clock()
